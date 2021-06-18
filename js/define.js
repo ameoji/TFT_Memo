1abbc51908c3e27b391a3b5632a1c4d13f3b78f7
t.yoshida <takuto.yoshida@souken.co.jp>
2021-06-18 16:20:17 +0900

    テンプレートのようなもの作成

---
+++ js/define.js
@@ -0,0 +1,127 @@
+const p_1 = '';
+const p_2 = '';
+const p_3 = '';
+const p_4 = '';
+const p_5 = '';
+const p_6 = '';
+const p_7 = '';
+const p_8 = '';
+const p_9 = '';
+const p_10 = '';
+
+const ITEM_TABLE = [
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_1, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_2, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_3, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_4, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_5, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_6, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_7, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_8, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_9, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_1,
+        'parts2': p_10, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_2,
+        'parts2': p_2, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_2,
+        'parts2': p_3, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_2,
+        'parts2': p_4, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_2,
+        'parts2': p_5, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_2,
+        'parts2': p_6, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_2,
+        'parts2': p_7, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_2,
+        'parts2': p_8, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_2,
+        'parts2': p_9, 
+    },
+    {
+        'name': '',
+        'img_src': '',
+        'parts1': p_2,
+        'parts2': p_10, 
+    },   
+]
