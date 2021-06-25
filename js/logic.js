383ab69077d495d5cb15ec02bcf5cef0529fd537
ameoji <ottimista0000@gmail.com>
2021-06-26 04:14:42 +0900

    入力欄の自動高さ調整

--- js/logic.js
+++ js/logic.js
@@ -392,3 +392,21 @@ function clearGroupBox(){
     }
   });
 }
+
+/*---------------------------------------
+メモ入力時の挙動
+---------------------------------------*/
+$(document).on('input', '.etc' , function(evt) {
+  if(evt.target.scrollHeight > evt.target.offsetHeight){   
+    $(evt.target).height(evt.target.scrollHeight);
+  }else{          
+    var lineHeight = Number($(evt.target).css("lineHeight").split("px")[0]);
+    while (true){
+        $(evt.target).height($(evt.target).height() - lineHeight); 
+        if(evt.target.scrollHeight > evt.target.offsetHeight){
+            $(evt.target).height(evt.target.scrollHeight);
+            break;
+        }
+    }
+  }
+});
